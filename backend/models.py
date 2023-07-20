42f9842a3c3c6035b8ec7125b3ca5ebc8e4eea26
zeemohamed7 <zainabshmohamed7@gmail.com>
2023-07-20 12:25:52 +0300

    add models and token_auth

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from token_auth.models import User
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    condition = models.CharField(max_length=255)
+    description = models.CharField(max_length=600)
+    image = models.CharField(max_length=255)
+    starting_bid = models.IntegerField()
+    category =  models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Bid(models.Model):
+    bid_amount = models.IntegerField()
+    bidder = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
