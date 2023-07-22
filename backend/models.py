247b545ef42c6ed38307e28a0303d0523175468a
Salman Murtaza <Salman.Murtaza.Minhas@outlook.com>
2023-07-22 20:52:30 +0300

    commented out the image from models.py, just uncomment it if needed

--- backend/models.py
+++ backend/models.py
@@ -17,9 +17,9 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     condition = models.CharField(max_length=255)
     description = models.CharField(max_length=600)
-    image = models.CharField(max_length=255)
+    # image = models.CharField(max_length=255)
     starting_bid = models.IntegerField()
-    # category =  models.ForeignKey(Category, related_name='user', on_delete=models.CASCADE)
+    category =  models.ForeignKey(Category, related_name='user', on_delete=models.CASCADE)
     owner = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
 
 
