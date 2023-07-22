e01622230d06863853cade10d8d8a8b23bdda990
zeemohamed7 <zainabshmohamed7@gmail.com>
2023-07-22 23:28:39 +0300

    add product detail page

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import ItemCreateView, ItemListView
+from .views import ItemCreateView, ItemListView, ItemDetailsView, place_bid
+
+# from .views import get_item_details
+
 
 
 urlpatterns = [
     # path(url path, what to run)
     path('api/items/create/', ItemCreateView.as_view()),
     path('api/items/', ItemListView.as_view()),
+    # path('/api/items/detail/${props.itemId}', get_item_details, name='get_item_details'),
+    path('api/items/detail/<pk>', ItemDetailsView.as_view()),
+    path('/api/items/bid', place_bid)
+
 ]
