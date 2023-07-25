e51a14c178b412373c37ce6eb63117d45bfe0c3e
Salman Murtaza <Salman.Murtaza.Minhas@outlook.com>
2023-07-25 14:27:44 +0300

    user model, update api, detail api

--- token_auth/models.py
+++ token_auth/models.py
@@ -3,4 +3,13 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    address = models.CharField(max_length = 200)
+    profile_image_url = models.CharField(max_length=600)
+    email_address = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    city = models.CharField(max_length=60)
+    block_number = models.CharField(max_length=50)
+    road_number = models.CharField(max_length=50)
+    house_number = models.CharField(max_length=50)
+    money = models.CharField(max_length=10, default='100')
--- token_auth/views.py
+++ token_auth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView, UpdateAPIView, CreateAPIView, ListAPIView
+from .models import User
+from .serializer import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+
+class UserUpdateView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
